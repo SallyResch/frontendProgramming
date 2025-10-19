f8714297b3a493018d1adf2692ef6c1b6ba07d19
Sally <sallyresch27@gmail.com>
2025-10-19 16:37:47 +0200

    added more content to javascript intro html

---
+++ JavaScript/jsIntro/js/ifstatements.js
@@ -0,0 +1,33 @@
+//alert("yo!");
+/*let myNumber = 5;
+let myName = "Sally";
+if (typeof myNumber === 'string') {
+    console.log("Its true");
+}
+else {
+    console.log("Its not true");
+}
+
+if (myName === "Sally") {
+    console.log("Hi " + myName);
+}
+else {
+    console.log("Noone here named " + myName);
+
+}
+
+if (myNumber > 10) {
+    console.log("Number is bigger then 10");
+}
+else {
+    console.log("Number is not bigger then 10");
+}
+*/
+
+const character = "Nyelle";
+const weapon = "Sword";
+console.log(`One grey day, ${character} was walking in to a cave`);
+
+if (weapon === "Sword") {
+    console.log(`She was swinging her ${weapon} and whistling, it was a good day for an adventure`);
+}
