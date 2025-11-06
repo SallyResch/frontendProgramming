97b107ccbec0c1de46c55b0856cca808b3e5bd02
Sally <sallyresch27@gmail.com>
2025-11-06 10:12:54 +0100

    added files for number guessing game and method lecture

---
+++ JavaScript/methods/script.js
@@ -0,0 +1,19 @@
+//CALLBACK FUNCTIONS
+/*
+const sayName = name => {
+    return (`Hello ${name}`)
+}
+
+const sayGoodbye = name => {
+    return (`Goodbye ${name}`)
+}
+
+const getName = (callbackFunction) => {
+    let userName = prompt("Enter your name");
+    let response = callbackFunction(userName);
+    console.log(`This is the response: ${response}`)
+}
+getName(sayName);
+getName(sayGoodbye);
+*/
+
